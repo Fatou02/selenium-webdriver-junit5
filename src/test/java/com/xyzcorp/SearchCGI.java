c390781bda8fc628f291fcfa885920e59c28da33
Fatou Diouf <ndeyefatou00@outlook.com>
2022-02-09 08:55:14 -0500

    added things

--- src/test/java/com/xyzcorp/SearchCGI.java
+++ src/test/java/com/xyzcorp/SearchCGI.java
@@ -1,56 +1,70 @@
 package com.xyzcorp;
 
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import io.cucumber.java.AfterAll;
-import io.cucumber.java.BeforeAll;
-
-
-public class SearchCGI{
-
-    
-private static ChromeDriver chromeDriver;
-
-    @BeforeAll
-    public static void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\fatou.diouf\\chrome-drivers\\98\\chromedriver_win32\\chromedriver.exe");
-
-                chromeDriver = new ChromeDriver();
-       }
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 
-       @AfterAll
-       public static void cleanup(){
-           chromeDriver.close();
-       }
+public class SearchCGI {
 
-       @Test
-       public void searchingInCGI() {
-         chromeDriver.get("https://www.cgi.com/en");
-         chromeDriver.manage().window().setSize(new Dimension(1936, 1056));
-         chromeDriver.findElement(By.linkText("EN")).click();
-         chromeDriver.findElement(By.linkText("FR")).click();
-         chromeDriver.findElement(By.linkText("Gouvernements")).click();
-         chromeDriver.findElement(By.cssSelector(".search-icon-btn")).click();
-         chromeDriver.findElement(By.cssSelector(".fa-close")).click();
-         chromeDriver.findElement(By.linkText("Contactez-nous")).click();
-         chromeDriver.findElement(By.name("name")).sendKeys("Fatou");
-         chromeDriver.findElement(By.name("company")).sendKeys("CGI");
-         chromeDriver.findElement(By.name("email")).sendKeys("abcd");
-         chromeDriver.findElement(By.name("phone")).sendKeys("7");
-         chromeDriver.findElement(By.name("relationship_to_cgi[select]")).click();
+    WebDriver chromeDriver;
 
+    @BeforeEach
+    void setup() {
+        chromeDriver = WebDriverManager.chromedriver().create();
+    }
 
-       }
+    @AfterEach
+    void teardown() {
+        chromeDriver.quit();
+    }
 
+    @Test
+    public void searchingInCGI() {
+        chromeDriver.get("https://www.cgi.com/fr");
+        //chromeDriver.manage().window().setSize(new Dimension(1936, 1056));
+
+        chromeDriver.findElement(By.xpath("//*[@id='popup-buttons']/button[1]")).click();
+
+        chromeDriver.manage().window().maximize();
 
 
+        //chromeDriver.manage().window().setPosition(new Point(0, 0));
 
+        //chromeDriver.findElement(By.cssSelector("#block-languageswitcher > ul > li.en.is-active > a")).click();
+        //chromeDriver.findElement(By.cssSelector("#block-languageswitcher > ul > li.fr > a")).click();
+        /*chromeDriver.findElement(By.linkText("Gouvernements")).click();
+        chromeDriver.findElement(By.cssSelector(".search-icon-btn")).click();
+        chromeDriver.findElement(By.cssSelector(".fa-close")).click();*/
 
+        WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(10));
 
+        WebElement contactezNous = wait.until(ExpectedConditions
+                .presenceOfElementLocated(By.linkText("Contactez-nous")));
 
+                contactezNous.click();
+
+        //chromeDriver.findElement(By.linkText("Contactez-nous")).click();
+        chromeDriver.findElement(By.name("name")).sendKeys("Fatou");
+        chromeDriver.findElement(By.name("company")).sendKeys("CGI");
+        chromeDriver.findElement(By.name("email")).sendKeys("abcd");
+        chromeDriver.findElement(By.name("phone")).sendKeys("7");
+        chromeDriver.findElement(By.name("relationship_to_cgi[select]")).click();
+        {
+            WebElement dropdown = chromeDriver.findElement(By.name("relationship_to_cgi[select]"));
+            dropdown.findElement(By.xpath("//option[. = 'Nouveau fournisseur']")).click();
+        }
+        chromeDriver.findElement(By.name("message")).click();
+        chromeDriver.findElement(By.name("message")).sendKeys("jfiovjrg");
+        chromeDriver.findElement(By.cssSelector(".ui-button:nth-child(1)")).click();
+    }
 
 }
